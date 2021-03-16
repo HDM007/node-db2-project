fed00771bbd2bb5cad065c83433ecbd7d4ec260a
hdm007 <harry.mardirossian13@gmail.com>
2021-03-16 17:39:41 -0400

    schema created in migration 01

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,25 @@
 // DO YOUR MAGIC
+exports.up = function (knex) {
+    return knex.schema
+    //run createTable method on schema object from knex to generate a table,
+    .createTable('cars', table => {
+        //each of the below is a column on the table object.
+
+        table.increments('car_id');
+        //sets car id to auto-increment
+        table.text('vin').unique().notNullable();
+        //sets unique string, required.
+        table.text('make').notNullable();
+        //sets make, required, not unique.
+        table.text('model').notNullable();
+        //sets model, required, not unique.
+        table.integer("mileage").notNullable();
+        table.text('title');
+        table.text('transmission');      
+    })
+};
+
+exports.down = function (knex) {
+    return knex.schema
+    .dropTableIfExists('cars')
+}
