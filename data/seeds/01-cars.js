9b96127869d1290a10fce8a0a41e84c92aa200f9
hdm007 <harry.mardirossian13@gmail.com>
2021-03-16 17:56:06 -0400

    added seed file, added scripts, ran initial migration to test seed

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,10 @@
 // STRETCH
+exports.seed = function (knex) {
+    return knex('cars').truncate()
+    .then(function () {
+        return knex('cars').insert([
+            {vin: "argybargyvin", make: "Mercedes", model: "C300", mileage: 10},
+            {vin: "supefrcar", make: "Chariot", model: "OG", mileage: 0},
+        ]);
+    });
+}
